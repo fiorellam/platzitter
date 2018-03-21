53a0cbe3fc1e73badfad12f8c92c0fb02e2629c7
Fiorella <fiorellaroma@hotmail.com>
2018-03-20 23:31:30 -0700

    Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //SE CREA UNA LISTA QUE ES N ARRAY QUE TENGA TWITTS
+  listado:Array<twitt> = [
+      {img:'http://www.impactoedu.com/uploads/4/6/1/1/46118149/7484265.jpg?275', nick_name:'@freddier', text:'platzitter es lo mas'},
+      {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+      {img:'https://avatars1.githubusercontent.com/u/5982204?s=460&v=4', nick_name:'@jorgeucano', text:'test 1234'}
+    ];
 
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface twitt{
+  img:string; //AVATAR
+  nick_name:string; //NICKNAME
+  tect:string;
 }
